00be48e251cd72c0c6286cf7e4920ed1bd7742e6
tafita37 <118960434+tafita37@users.noreply.github.com>
2025-05-23 03:18:43 +0300

    feat(consultation): create consultation model and compute total price

--- models/Consultation.py
+++ models/Consultation.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class Consultation(models.Model):
     _name = 'medicalconsulting.consultation'
@@ -9,3 +9,14 @@ class Consultation(models.Model):
     consultation_date = fields.Datetime(string='Consultation Date', default=fields.Datetime.now)
     diagnosis = fields.Text(string='Diagnosis')
     consultation_line_ids = fields.One2many('medicalconsulting.consultation.line', 'consultation_id', string='Consultation Lines')
+
+    total_price = fields.Float(string="Total Price", compute='_compute_total_price')
+
+    @api.depends('consultation_line_ids.quantity')
+    def _compute_total_price(self):
+        for record in self:
+            total = 0.0
+            for line in record.consultation_line_ids:
+                total += (line.medication_id.unit_price or 0.0) * (line.quantity or 0.0)
+            record.total_price = total
+
