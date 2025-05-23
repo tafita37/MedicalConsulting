6412158879b98688c862db080922671ce3864b6d
tafita37 <118960434+tafita37@users.noreply.github.com>
2025-05-23 04:19:34 +0300

    feat(consultation) : add details to consultation creation

--- models/Consultation.py
+++ models/Consultation.py
@@ -1,4 +1,6 @@
+from datetime import datetime
 from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 class Consultation(models.Model):
     _name = 'medicalconsulting.consultation'
@@ -12,6 +14,13 @@ class Consultation(models.Model):
 
     total_price = fields.Float(string="Total Price", compute='_compute_total_price')
 
+    state = fields.Selection(
+        [
+            ('not_processed', 'Not processed'), ('in_progress', 'In progress'), ('done', 'Done')
+        ], string='State', default='not_processed', required=True
+    )
+
+
     @api.depends('consultation_line_ids.quantity')
     def _compute_total_price(self):
         for record in self:
@@ -19,4 +28,25 @@ class Consultation(models.Model):
             for line in record.consultation_line_ids:
                 total += (line.medication_id.unit_price or 0.0) * (line.quantity or 0.0)
             record.total_price = total
+
+    @api.constrains('doctor_id', 'state', 'consultation_date')
+    def _check_doctor_max_consultations(self):
+        for record in self:
+            if record.state == 'in_progress' and record.consultation_date:
+                date_start = record.consultation_date.date()
+                datetime_start = datetime.combine(date_start, datetime.min.time())
+                datetime_end = datetime.combine(date_start, datetime.max.time())
+
+                count = self.search_count([
+                    ('doctor_id', '=', record.doctor_id.id),
+                    ('state', '=', 'in_progress'),
+                    ('consultation_date', '>=', datetime_start),
+                    ('consultation_date', '<=', datetime_end),
+                    ('id', '!=', record.id),
+                ])
+                if count >= 3:
+                    raise ValidationError(
+                        f"This doctor already has 3 consultations in progress on {date_start.strftime('%Y-%m-%d')}."
+                    )
+
 
